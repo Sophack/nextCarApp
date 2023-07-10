a8ef5dc63b838892fbe06a2b1f31f87612a825e0
SarafN <sarafnrealtor@gmail.com>
2023-07-10 15:09:34 -0400

    fixed type error

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image';
-import { CustomButtonProps }  from "@/types"
+import { CustomButtonProps }  from "@/types";
 
 //default all components are server side rendered 
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,13 @@
 "use client";
-import Image from "next/image"
-import CustomButton from "./CustomButton"
+import Image from "next/image";
+import { CustomButton } from "@/components";
 
 
 const Hero = () => {
 
     const handleScroll = () => {
     }
+    
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -23,6 +24,7 @@ const Hero = () => {
       title="Explore Cars"
       containerStyles="bg-primary-blue text-white rounded-full mt-10"
       handleClick={handleScroll}
+      btnType="button"
       />
 
       </div>
