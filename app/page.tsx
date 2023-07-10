bd8943de0892a211f559f9ef801e1c1b41049d66
SarafN <sarafnrealtor@gmail.com>
2023-07-10 17:16:25 -0400

    added searchbar & manufacturer

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter />
-            <CustomFilter />
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year'  />
           </div>
     </div>
     </div>
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,16 @@
+"use client";
+import { Combobox, Transition } from "@headlessui/react"
 import { SearchManufacturerProps } from "@/types"
 
 
 const SearchManufacturer = ({ manufacturer, setManufacturer} : SearchManufacturerProps) => {
   return (
-    <div>
-      SearchMAn
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+        
+        </div>
+      </Combobox>
     </div>
   )
 }
